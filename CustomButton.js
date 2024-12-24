503bf26f6c619247e87fbff67cc252a20991ef8f
Jeferson Alan <cabello_10@live.com>
2024-12-24 20:45:49 -0300

    Create CustomButton.js

---
+++ CustomButton.js
@@ -0,0 +1,26 @@
+// components/CustomButton.js - Exemplo de Componente Simples: Um componente simples de botão que pode ser usado em qualquer tela:
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+export default function CustomButton({ title, onPress }) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#1abc9c',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginVertical: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
